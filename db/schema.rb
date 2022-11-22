528f8bbbb2020a194f5e575c42c4b4fa606f8eb4
AbelG101 <abelgebeyehu101@gmail.com>
2022-11-22 23:37:21 +0300

    Add posts as foregin key to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_194802) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_203534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,9 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_194802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.index ["author"], name: "index_comments_on_author"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post"], name: "index_comments_on_post"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -61,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_194802) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
