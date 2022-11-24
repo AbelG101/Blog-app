53bcfcf3b56c8d1554db79d13fe458fdecb1ed58
AbelG101 <abelgebeyehu101@gmail.com>
2022-11-24 21:32:13 +0300

    Add call to update counter function after save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_count
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
   def update_comments_count
