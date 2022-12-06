dd1923e0f0d2c8ae2f88ed779a15289513cc9c22
Duane David <duanedave17@gmail.com>
2022-12-06 08:05:01 -0800

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
   end
-
-  def current_user
-    @current_user = User.find(params[:user_id])
-  end
 end
